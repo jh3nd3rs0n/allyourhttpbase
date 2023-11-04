d60b2b13c57344a7ae1a34f10b3994a7c93e8bf0
Jonathan K. Henderson <j0n4th4n.h3nd3rs0n@gmail.com>
2023-11-04 15:25:17 -0400

    updated naming

---
+++ src/main/java/com/github/jh3nd3rs0n/allyourhttpbase/http1dot1/StartLine.java
@@ -0,0 +1,9 @@
+package com.github.jh3nd3rs0n.allyourhttpbase.http1dot1;
+
+public abstract class StartLine {
+	
+	StartLine() { }
+	
+	public abstract byte[] toByteArray();
+	
+}
---
+++ src/main/java/com/github/jh3nd3rs0n/allyourhttpbase/http1dot1/StringConstants.java
@@ -0,0 +1,14 @@
+package com.github.jh3nd3rs0n.allyourhttpbase.http1dot1;
+
+final class StringConstants {
+	
+	public static final String ANY_WHITESPACE_CHAR_REGEX = "[\\h\\s\\v]";
+	public static final String CRLF = "\r\n";
+	public static final String HTTP_VERSION = "HTTP/1.1";
+	public static final String HTTP_VERSION_REGEX = "^HTTP/[\\d]\\.[\\d]$";
+	public static final String UNACCEPTABLE_WHITESPACE_CHAR_REGEX = 
+			"[[\\h\\s\\v]&&[^ \\t]]";
+
+	private StringConstants() { }
+	
+}
